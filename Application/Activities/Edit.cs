1514727ac5489a1036e3b9b574b50864072894b0
Ayush Vyas <sidharatvyas26@gmail.com>
2023-12-31 13:25:40 +0530

    End of section 10

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,7 @@
+using Application.Core;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -7,12 +9,20 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -22,13 +32,18 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                var activity = await _context.Activities.FindAsync(new object[] { request.Activity.Id }, cancellationToken: cancellationToken);
 
-                _mapper.Map(request.Activity,activity);
+                if (activity == null) return null;
 
-                await _context.SaveChangesAsync();
+                _mapper.Map(request.Activity, activity);
+
+                var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+                if (!result) return Result<Unit>.Failure("Failed to update activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,6 +3,7 @@ import { StoreContext, store } from "./app/stores/store";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/Routes";
 import "react-calendar/dist/Calendar.css";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./app/layout/styles.css";
 
 ReactDOM.render(
